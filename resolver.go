478114a72501b2c443e99f82b928857f10a90b26
0xAllan <mwaranguallan345@gmail.com>
2024-01-29 16:13:30 +0300

    add resolver function

---
+++ resolver.go
@@ -0,0 +1,15 @@
+package main
+
+type Mapper struct {
+	Domain string
+	IPV4   string
+	IPV6   string
+}
+
+func GetDomain(ip string) (string, error) {
+	return "", nil
+}
+
+func GetIp(domain string) (string, error) {
+	return "", nil
+}
